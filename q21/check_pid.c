f8527e639dd3c55d82806f705877c21048e394a9
Amit Ran <57709369+amitRan109@users.noreply.github.com>
2020-08-12 08:19:35 -0400

    Add files via upload

---
+++ q21/check_pid.c
@@ -0,0 +1,32 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <signal.h>  
+#include <errno.h> 
+#include <string.h>
+
+void _kill (pid_t pid) {
+    kill(pid,0);
+    switch(errno) {
+        case 0: 
+            printf("Process %d exists.\n",pid);
+            break;
+        case ESRCH: 
+            printf("Process %d does not exists.\n",pid); 
+            break;
+        case EPERM: 
+            printf("Process %d exists but we have no permission.\n",pid);
+            break;
+    }
+}
+
+int main (int argc, char* argv[]){
+    if (argc < 2) {
+        printf ("error: enter number\n");
+    }
+    else {
+        pid_t pid = atoi(argv[1]);
+        _kill(pid);
+    }
+    
+    exit(EXIT_SUCCESS);
+}
